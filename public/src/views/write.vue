2db8a4bdf8a5371b7ab63ed2b54f034d7ea3b096
taoxin <taoxin167@gmail.com>
2016-07-11 22:55:30 +0800

    引用showdown，使用mackdown在线发布文章，调整相应格式

--- public/src/views/write.vue
+++ public/src/views/write.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <div class="write-left">
-            <textarea class="write-textarea" v-model="content" @keyup="contentTrans"></textarea>
+        <div class="row">
+            <div class="write-left col-lg-offset-1 col-lg-5 col-md-5 col-sm-5">
+                <textarea class="write-textarea" v-model="content" @keyup="contentTrans"></textarea>
+            </div>
+            <div class="write-right col-lg-6 col-md-6 col-sm-6" id="write-right">
+                <!-- {{htmlContent}} -->
+            </div>
         </div>
-        <div class="write-right">
-            {{htmlContent}}
+        <div class="row">
+            <div class="col-lg-offset-1 btn-row">
+                <button class="btn btn-primary">提交</button>
+                <button class="btn btn-warn">取消</button>
+            </div>
         </div>
     </div>
 </template>
@@ -21,42 +29,49 @@ export default{
         };
     },
     ready(){
+        document.title = '发表文章－－管理后台';
     },
     watch:{
 
-       
+
     },
     computed:{
     },
     methods:{
          // mackdown转换为html
-        contentTrans(){
+         contentTrans(){
             const text = this.content;
-            const converter = new Showdown.converter();
+            const converter = new showdown.Converter();
             const html = converter.makeHtml(text);
             this.htmlContent = html;
+            document.getElementById('write-right').innerHTML = html;
         }
     }
 };
 </script>
 <style type="text/css">
     .write-left{
-        width: 500px;
         height: 600px;
-        float: left;
-        border: 1px solid #333333;
-        overflow: auto;
+        margin-top: 10px;
     }
-    .write-textarea{
+    .write-textarea,
+    .write-textarea:focus{
         width: 100%;
         height: 100%;
         resize: none;
+        outline: none;
+        overflow: auto;
     }
     .write-right{
-        float: right;
-        width: 500px;
         height: 600px;
-        border: 1px solid #333333;
         overflow: auto;
+        margin-top: 10px;
+        border-left: 1px solid;
+    }
+    .row{
+        margin: 0px;
+    }
+    .btn-row{
+        margin-top:10px;
     }
 </style>
