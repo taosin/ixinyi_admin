d7da7b0d454fc8696455d1db1a1ed66652c166fa
taoxin <taoxin167@gmail.com>
2016-07-12 23:27:16 +0800

    调整login页面样式

--- public/src/views/login.vue
+++ public/src/views/login.vue
@@ -8,13 +8,13 @@
 				</mdinput>
 			</div>
 			<div class="login-input">
-				<mdinput :value.sync="password">
+				<mdinput :value.sync="password" @keyup.enter="handlerLogin">
 					<label slot="input-lable">Password</label>
 				</mdinput>
 			</div>
-			<div class="login-input">
-				<button class="btn btn-primary">登录</button>
-				<button class="btn btn-warn">取消</button>
+			<div class="login-btn">
+				<button class="btn btn-login common-btn" @click="handlerLogin">登录</button>
+				<button class="btn btn-cancel common-btn">取消</button>
 			</div>
 		</div>
 	</div>
@@ -32,12 +32,19 @@
 			};
 		},
 		ready(){
+			document.title = '登录－－管理后台';
 		},
 		watch:{
 		},
 		computed:{
 		},
 		methods:{
+			handlerLogin(){
+				// if(this.username && this.password){
+				// 	console.info('login success');
+				// }
+				alert(111);
+			}
 		}
 	};
 </script>
@@ -46,7 +53,7 @@
 		/*text-align: center;*/
 		margin:150px auto;
 		width: 300px;
-		height: 300px;
+		height: 350px;
 		border:1px solid #000000;
 		padding:60px;
 		border-radius: 3px;
@@ -55,5 +62,31 @@
 		width: 100%;
 		height: 80px;
 		float: left;
+		padding: 30px 0;
+	}
+	.login-btn{
+		width: 100%;
+		height: 80px;
+		float: left;
+		margin-top: 60px;
+		text-align: center;
+	}
+	.common-btn{
+		outline: none;
+		border-radius: 0;
+		padding: 5px 21px;
+	}
+	.common-btn:hover,
+	.common-btn:focus,
+	.common-btn:active{
+		outline: none;
+		border-radius: 0;
+		padding: 5px 21px;
+		background-color: #000000;
+		color: #ffffff;
+	}
+	.btn-login{
+		background: #ffffff;
+		color: #000000;
 	}
 </style>
