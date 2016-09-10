2beca20346a09da4beae47cada34046dc0b7fc76
taoxin <taoxin167@gmail.com>
2016-09-10 19:05:13 +0800

    修改菜单样式，加入logo

--- public/src/views/menu.vue
+++ public/src/views/menu.vue
@@ -1,17 +1,27 @@
 <!-- 顶部菜单 -->
 <template>
 	<div class="menu">
+		<div class="logo">
+			<a href="#"><img :src="logo" title="Sin"></a>
+		</div>
 		<div class="main">
 			<ul>
 				<li><a href="#">首页</a></li>
 				<li><a>技术</a></li>
 				<li><a>生活</a></li>
 				<li><a>关于我</a></li>
-				<li><a>关于本站</a></li>
+				<li><a>时间轴</a></li>
 			</ul>
 		</div>
 	</div>
 </template>
 <script>
 	import './../../static/css/menu.scss';
+	export default{
+		data(){
+			return{
+				logo:'http://blog.xinsay.cn/luffy.jpg'
+			};
+		}
+	};
 </script>
