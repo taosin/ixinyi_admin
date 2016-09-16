2689e5403d3338bfc7b00cd88cd4f15b488f986b
taoxin <taoxin167@gmail.com>
2016-09-16 22:12:15 +0800

    修改样式

--- public/src/views/menu.vue
+++ public/src/views/menu.vue
@@ -1,17 +1,19 @@
 <!-- 顶部菜单 -->
 <template>
 	<div class="menu">
-		<div class="logo">
-			<a href="#"><img :src="logo" title="Sin"></a>
-		</div>
-		<div class="main">
-			<ul>
-				<li><a href="#/index/blogs">Home</a></li>
-				<li><a href="#/index/techs">技术</a></li>
-				<!-- <li><a href="#/index/life">生活</a></li> -->
-				<li><a>关于我</a></li>
-				<!-- <li><a>时间轴</a></li> -->
-			</ul>
+		<div class="div">
+			<div class="logo">
+				<a href="#"><img :src="logo" title="Sin"></a>
+			</div>
+			<div class="main">
+				<ul>
+					<li><a href="#/index/blogs">Home</a></li>
+					<!-- <li><a href="#/index/techs">技术</a></li> -->
+					<!-- <li><a href="#/index/life">生活</a></li> -->
+					<li><a>关于我</a></li>
+					<!-- <li><a>时间轴</a></li> -->
+				</ul>
+			</div>
 		</div>
 	</div>
 </template>
